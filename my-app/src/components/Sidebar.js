9d46f696c6ff1e90d3e9e108bdade49fc18d971d
Conor Timmis <mctto@protonmail.com>
2024-08-05 20:30:07 +0000

    Create Sidebar Component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Sidebar = () => (
+    <div>
+    <a href = "#." target= "_blank">My Photos</a>
+    <a href = "#." target= "_blank">My Illustrations</a>
+    <a href = "#." target= "_blank">My Paintings</a>
+    </div>
+);
+
+export default Sidebar;
