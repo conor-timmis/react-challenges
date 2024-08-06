fdd1de9fec5c653ed01df0605267eb6848ad86e3
Conor Timmis <mctto@protonmail.com>
2024-08-06 17:54:03 +0000

    Add button handling to Component Greeting

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -10,12 +10,19 @@ class StatefulGreeting extends React.Component {
         };
     }
 
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye"
+        });
+        console.log(this.state.introduction);
+    }
+
     render() {
         return (
-        <div>
-        <h1>{this.state.introduction} {this.props.greeting}</h1>
-        <button>{this.state.buttonText}</button>
-        </div>
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
         )
     }
 }
