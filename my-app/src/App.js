4df8a474e76d2e207309ed1b922056405637b984
Conor Timmis <mctto@protonmail.com>
2024-08-06 16:19:21 +0000

    Add StatefulGreeting to App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
+      <StatefulGreeting />
       <Sidebar />
     </div>
   );
