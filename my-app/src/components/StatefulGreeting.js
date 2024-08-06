50dc740bce9edf05280418a8ac6f47dfb350fffd
Conor Timmis <mctto@protonmail.com>
2024-08-06 19:20:55 +0000

    Refactor to functional setState

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -10,50 +10,42 @@ class StatefulGreeting extends React.Component {
     };
   }
 
-  handleClick() {
-    this.setState(
-        {
-      introduction: this.state.introduction === "Hello" ? "Goodbye" : "Hello",
-      buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
-    },
-    () => {
-        console.log('new state', this.state.introduction);
-        console.log('new state', this.state.buttonText);
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+        console.log("Previous State:", prevState);
+        console.log("Previous Props:", prevProps);
+        return {
+            introduction:
+            prevState.introduction === "Hello" ? "Goodbye" : "Hello",
+            buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+        };
+        });
     }
-    );
-  }
 
-  increment() {
-    this.setState({
-        count: this.state.count + 1
-    })
-    console.log(this.state.count)
-  }
-
-  incrementFive() {
-    this.increment()
-    this.increment()
-    this.increment()
-    this.increment()
-    this.increment()
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>
-          {this.state.introduction} {this.props.greeting}
-        </h1>
-        <button onClick={() => this.handleClick()}>
-          {this.state.buttonText}
-        </button>
-        <button onClick={() => this.incrementFive()}>
-            Increment Five
-        </button>
-        <p>You've clicked {this.state.count} times</p>
-      </div>
-    );
-  }
-}
+    increment() {
+    this.setState((prevState, prevProps) => {
+        console.log("Previous State:", prevState);
+        console.log("Previous Props:", prevProps);
+        return {
+        count: prevState.count + 1,
+        };
+    });
+    }
+    
+    render() {
+        return (
+            <div>
+            <h1>
+                {this.state.introduction} {this.props.greeting}
+            </h1>
+            <button onClick={() => this.handleClick()}>
+                {this.state.buttonText}
+            </button>
+            <button onClick={() => this.increment()}>Increment</button>
+            <p>You've clicked {this.state.count} times</p>
+            </div>
+        );
+        }
+    }
 
 export default StatefulGreeting;
