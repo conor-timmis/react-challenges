e3e715b9ac2cc44d75ad851aeac93f55969776a6
Conor Timmis <mctto@protonmail.com>
2024-08-08 20:30:33 +0000

    Convert methods as props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
-import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 
 function App() {
-  return (
-    <div className="App">
-      <ConditionalRenderingClass />
-      <ConditionalRenderingFunctional connected={true} />
-    </div>
-  );
+  return <div className="App">
+      <MethodsAsPropsParent />
+  </div>;
 }
 
 export default App;
