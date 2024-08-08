b6c3eb311f83138bfc9a202db71685f08d8804a2
Conor Timmis <mctto@protonmail.com>
2024-08-08 20:11:58 +0000

    Refactor into smaller components for clarity

--- my-app/src/components/ConditionalRenderingClass.js
+++ my-app/src/components/ConditionalRenderingClass.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import UserMessage from "./UserMessage";
+import UserData from "./UserData";
 
 export class ConditionalRenderingClass extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isLoaded: true,
+      isLoaded: false,
       isLoggedIn: true,
     };
   }
@@ -13,19 +15,8 @@ export class ConditionalRenderingClass extends Component {
   render() {
     return (
       <div>
-        <h1>{this.state.isLoaded ? "Data loaded!" : "Loading..."}</h1>
-        {this.state.isLoggedIn ? (
-          <div>
-            <p>Welcome to the site! Please complete the steps below:</p>
-            <ol>
-              <li>Confirm your email</li>
-              <li>Complete your profile</li>
-              <li>Subscribe to the newsletter</li>
-            </ol>
-          </div>
-        ) : (
-          <p>Please sign in</p>
-        )}
+        <UserData isLoaded={this.state.isLoaded} />
+        <UserMessage isLoggedIn={this.state.isLoggedIn} />
       </div>
     );
   }
